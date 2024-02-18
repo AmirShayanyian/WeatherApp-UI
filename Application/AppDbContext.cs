ce0960210fbcae750a32671806f3503cfb6c6785
Amir Shayan <wahtamir12@gmail.com>
2024-02-18 23:11:48 +0330

    Completed appcontext to set entities

--- Application/AppDbContext.cs
+++ Application/AppDbContext.cs
@@ -1,8 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Application.Entities.BaseEntity;
+using Domain.Core.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options)
+    {
+        
+    }
     
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        var assemblies = typeof(BaseEntity).Assembly;
+        modelBuilder.AddAllEntities<BaseEntity>(assemblies);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IBaseEntity).Assembly);
+    }
 }
---
+++ Domain/Core/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Core.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void AddAllEntities<TBaseType>(this ModelBuilder modelBuilder, params Assembly[] assemblies)
+    {
+        var entityTypes = assemblies
+            .SelectMany(x => x.GetExportedTypes())
+            .Where(p => p is { IsPublic: true, IsAbstract: false } &&
+                        p.IsSubclassOf(typeof(TBaseType)));
+        foreach (var type in entityTypes)
+        {
+            modelBuilder.Entity(type);
+        }
+    }
+}
